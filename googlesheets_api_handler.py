34b716f31e621ad9183e69412ad8643c834415b3
Janet Chang <janetlaichang@gmail.com>
2017-02-22 17:38:04 -0800

    cutting extraneous, additional notation and testing to determine desired read/write command to change

--- googlesheets_api_handler.py
+++ googlesheets_api_handler.py
@@ -38,41 +38,42 @@ def get_credentials():
     #credential_path = 'C:\Users\ece03ht\.credentials\sheets.googleapis.com-python-quickstart.json'
     credential_path = os.path.join(credential_dir,
                                    'sheets.googleapis.com-drugshortagesweekly.json')
-    #store = 
     store = Storage(credential_path)
     credentials = store.get()
-    #if credentials don't exist or are invalid...
+    
+    #If credentials don't exist or are invalid...
+    #Make a json file with the appropriate information
     if not credentials or credentials.invalid:
-        #
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
         flow.user_agent = APPLICATION_NAME
-        print (flow.user_agent)
         if flags:
             credentials = tools.run_flow(flow, store, flags)
         else: # Needed only for compatibility with Python 2.6
             credentials = tools.run(flow, store)
         print('Storing credentials to ' + credential_path)
     return credentials
-    print (credentials)
+    print ("These are the credentials:", credentials)
 
 def main():
-    """Shows basic usage of the Sheets API.
-
-    Creates a Sheets API service object and prints the names and majors of
-    students in a sample spreadsheet:
-    https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-    """
     credentials = get_credentials()
-    #http and discoveryUrl are used in building the service object. discovery must be part of a module?
+    
+    #Google has a "Discovery document" that includes a top-level "resources" section
+    #that groups all the reources associated with the API
     http = credentials.authorize(httplib2.Http())
     discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?version=v4')
+    print ("This is the result of calling discovery:", discovery)
     service = discovery.build('sheets', 'v4', http=http, discoveryServiceUrl=discoveryUrl)
-
+    print ("This is the result of calling service:", service)
+    
     #https://sheets.googleapis.com/v4/spreadsheets/{spreadsheetId}
-    spreadsheetId = '1VOd_RJZozTm4JtJtvXQtcRzaqW9UsS3nKKwfBkiOLro'
-
+    spreadsheet_id = '1VOd_RJZozTm4JtJtvXQtcRzaqW9UsS3nKKwfBkiOLro'
     specified_range = 'MASTER!A1:D4'
-    result = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=specified_range).execute()
+    
+    #result = with the service object, call the method spreadsheets.values.get() and input the spreadsheet ID and range
+    #result is a JSON object, with nested cell key for "values"
+    result = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=specified_range).execute()
+    print ("This is the result of calling spreadsheets.values.get() on the service object:", result)
+
     values = result.get('values', [])
 
     if not values:
@@ -80,8 +81,7 @@ def main():
     else:
         print('Campaign, Position Type:')
         for each_row in values:
-            print('%s, %s' % (each_row[1], each_row[2]))
-
+            print(each_row[1], each_row[2], each_row[3])
 
 if __name__ == '__main__':
     main()
